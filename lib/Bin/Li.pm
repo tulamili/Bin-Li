fbaa59d4e06f44be5da8a2afe067d32f1ece056b
Toshiyuki Shimono <bin4tsv@gmail.com>
2021-05-25 22:39:32 +0900

    scripts に足したファイルに応じて、Changes README lib/Bin/li.pm の3個のファイルの書き換え

--- lib/Bin/Li.pm
+++ lib/Bin/Li.pm
@@ -1,6 +1,6 @@
 package Bin::Li ;
-our $VERSION = '0.100' ; 
-our $DATE = '2021-05-25T17:55+0900' ; 
+our $VERSION = '0.110' ; 
+our $DATE = '2021-05-25T22:30+0900' ; 
 
 =head1 NAME
 
@@ -17,11 +17,20 @@ The included commands are as follows.
 expskip     ; Only shows 1st, 10th, 100th, 1000th lines and so on.
 freq        ; Counts the frequencies of each different value that appears as one line.
 sampler     ; Randomly selected lines will be extracted. 
-cat-n       ; Alternative function of "cat -n". 
+cat-n       ; Alternative function of "cat -n". i.e. the space will become the tab character, not the sequence of the space characters. 
 chars2code  ; Each line is interpreted as the stream of binary codes. Hexagonal ASCII codes or Unicode will be shown.
 summing     ; Accumulative sum will be shown while reading each line.
 gzpaste     ; Unix `paste` funciton for multiple gzipped files. 
 
+linedigest  ; print each "digest" (hash value) of all the lines.
+idmaker     ; put the same id (number) on each "different" line values.
+uniq-c      ; Alternative of "uniq -c".
+wc-l        ; Alternative of "wc -l".
+headomit    ; a utility especially for multiple files so that the common first line values will be treated (omitted or reserved).
+lminusl     ; Given 2 files A and B, each file regarded to be a set of line values {a} or {b} for the two, {a∈A}\{b∈B} will be shown.
+alluniq     ; gives the infomation whether every line of a file contains all different or how the "multitudes" distributes. useful.
+digitdemog  ; shows the character distributions for each digits from the left (not the right) of all the lines.
+entrolines  ; calculates the "entropy" how each line values are different. 
 
 =head1 SEE ALSO
 
