fe8e170bf38b3ada845eb6fd1fc676c1a9b23ff7
Toshiyuki Shimono <bin4tsv@gmail.com>
2021-05-26 00:52:53 +0900

    lib/Bin/Li.pm の podにおけるエラーを解消。

--- lib/Bin/Li.pm
+++ lib/Bin/Li.pm
@@ -14,60 +14,58 @@ This module "Bin::Li" provides scripts for specific functions that deals "line-r
 
 =head1 DESCRIPTION
 
-=over 4
-
-=item * 
-
 The included commands are as follows. 
 
-=item 1 
-B<expskip>     ; Only shows 1st, 10th, 100th, 1000th lines and so on.
+=over 4
 
-=item 2 
-B<freq>        ; Counts the frequencies of each different value that appears as one line.
+=item *
+1. B<expskip>     ; Only shows 1st, 10th, 100th, 1000th lines and so on.
 
-=item 3 
-B<sampler>     ; Randomly selected lines will be extracted. 
+=item *
+2. B<freq>        ; Counts the frequencies of each different value that appears as one line.
 
-=item 4 
-B<cat-n>       ; Alternative function of "cat -n". i.e. the space will become the tab character, not the sequence of the space characters. 
+=item *
+3. B<sampler>     ; Randomly selected lines will be extracted. 
 
-=item 5 
-B<chars2code>  ; Each line is interpreted as the stream of binary codes. Hexagonal ASCII codes or Unicode will be shown.
+=item *
+4. B<cat-n>       ; Alternative function of "cat -n". i.e. the space will become the tab character, not the sequence of the space characters. 
 
-=item 6 
-B<summing>     ; Accumulative sum will be shown while reading each line.
+=item *
+5. B<chars2code>  ; Each line is interpreted as the stream of binary codes. Hexagonal ASCII codes or Unicode will be shown.
 
-=item 7 
-B<gzpaste>     ; Unix `paste` funciton for multiple gzipped files. 
+=item *
+6. B<summing>     ; Accumulative sum will be shown while reading each line.
+
+=item *
+7. B<gzpaste>     ; Unix `paste` funciton for multiple gzipped files. 
 
 
-=item 8 
-B<linedigest>  ; print each "digest" (hash value) of all the lines.
+=item *
+8. B<linedigest>  ; print each "digest" (hash value) of all the lines.
 
-=item 9 
-B<idmaker>     ; put the same id (number) on each "different" line values.
+=item *
+9. B<idmaker>     ; put the same id (number) on each "different" line values.
 
-=item 0 
-B<uniq-c>      ; Alternative of "uniq -c".
+=item *
+10. B<uniq-c>      ; Alternative of "uniq -c".
 
-=item 1 
-B<wc-l>        ; Alternative of "wc -l".
+=item *
+11. B<wc-l>        ; Alternative of "wc -l".
 
-=item 2 
-B<headomit>    ; a utility especially for multiple files so that the common first line values will be treated (omitted or reserved).
+=item *
+12. B<headomit>    ; a utility especially for multiple files so that the common first line values will be treated (omitted or reserved).
 
-=item 3 
-B<lminusl>     ; Given 2 files A and B, each file regarded to be a set of line values {a} or {b} for the two, {a∈A}\{b∈B} will be shown.
+=item *
+13. B<lminusl>     ; Given 2 files A and B, each file regarded to be a set of line values {a} or {b} for the two, {a∈A}\{b∈B} will be shown.
 
-=item 4 
-B<alluniq>     ; gives the infomation whether every line of a file contains all different or how the "multitudes" distributes. useful.
+=item *
+14. B<alluniq>     ; gives the infomation whether every line of a file contains all different or how the "multitudes" distributes. useful.
 
-=item 5 
-B<digitdemog>  ; shows the character distributions for each digits from the left (not the right) of all the lines.
+=item *
+15. B<digitdemog>  ; shows the character distributions for each digits from the left (not the right) of all the lines.
 
-=item 6 
-B<entrolines>  ; calculates the "entropy" how each line values are different. 
+=item *
+16. B<entrolines>  ; calculates the "entropy" how each line values are different. 
 
 =back
 
